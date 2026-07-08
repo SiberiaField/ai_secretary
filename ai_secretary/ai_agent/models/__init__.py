21f78958ea196ff7d8f43ad35994fc8c7b794de5
buklinfur <buklinfur@outlook.com>
2026-07-08 13:56:12 +0700

    Updated imports

--- ai_secretary/ai_agent/models/__init__.py
+++ ai_secretary/ai_agent/models/__init__.py
@@ -1,7 +1,9 @@
-from .base import Model
-from .hugging_face import HuggingFaceModel
+from ai_agent.models.base import Model
+from ai_agent.models.hugging_face import HuggingFaceModel
+from ai_agent.models.gigachat_api import GigaChatAPIModel
 
 __all__ = [
     "Model",
-    "HuggingFaceModel"
+    "HuggingFaceModel",
+    "GigaChatAPIModel"
 ]
