32d1a163d5645367ef3a06f103fbe37738aa3215
Vladislav Polyanskii <v.polyanskii@g.nsu.ru>
2026-07-09 21:31:28 +0700

    updated imports

--- ai_secretary/task_managers/imap.py
+++ ai_secretary/task_managers/imap.py
@@ -5,7 +5,7 @@ from email.header import Header
 import uuid
 from typing import Dict, Any, List, Optional
 
-from ai_secretary.task_managers.base import Task, TaskManager, TaskStatus, TaskNotFoundError
+from task_managers.base import Task, TaskManager, TaskStatus, TaskNotFoundError
 
 
 class IMAPTaskManager(TaskManager):
